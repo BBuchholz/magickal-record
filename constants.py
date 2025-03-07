440ab165537bab6a864e1ab395f888589974e265
Brent Buchholz <brentbuchholz@gmail.com>
2025-03-07 04:07:41 -0600

    Update constants file with config folder information in comments

--- constants.py
+++ constants.py
@@ -1,1 +1,5 @@
+# keep a small subset for testing here
 READ_FROM_FOLDER = "~/obsidianTestFolder"
+# store external folder addresses in md files 
+# in this folder, so they stay out of github
+CONFIG_FOLDER = "~/obsidianConfig"
