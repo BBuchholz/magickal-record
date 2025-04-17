b9acd76df55036bf04cb560577ae7b8544d055c8
Brent Buchholz <brentbuchholz@gmail.com>
2025-04-16 21:11:12 -0500

    Add hard coded aliases to get all tests to pass

--- chronomancer.py
+++ chronomancer.py
@@ -1,4 +1,14 @@
 from sabbat import Sabbat
+from constants_sabbats import (
+  LTH_ALIASES,
+  BEL_ALIASES,
+  LMS_ALIASES,
+  MBN_ALIASES,
+  SMH_ALIASES,
+  YUL_ALIASES,
+  IMB_ALIASES,
+  OST_ALIASES,
+)
 
 class Chronomancer:
   def __init__(self):
@@ -6,9 +16,60 @@ class Chronomancer:
 
   def get_sabbat(self, letter_code):
     match letter_code:
+      case "BEL25":
+        aliases = BEL_ALIASES
+        sbt = Sabbat("BEL", aliases, 2025)
+        return sbt
       case "LTH25":
-        aliases = [
-          "Litha",
-        ]
+        aliases = LTH_ALIASES
         sbt = Sabbat("LTH", aliases, 2025)
         return sbt
+      case "LMS25":
+        aliases = LMS_ALIASES
+        sbt = Sabbat("LMS", aliases, 2025)
+        return sbt
+      case "MBN25":
+        aliases = MBN_ALIASES
+        sbt = Sabbat("MBN", aliases, 2025)
+        return sbt
+      case "SMH25":
+        aliases = SMH_ALIASES
+        sbt = Sabbat("SMH", aliases, 2025)
+        return sbt
+      case "YUL25":
+        aliases = YUL_ALIASES
+        sbt = Sabbat("YUL", aliases, 2025)
+        return sbt
+      case "IMB26":
+        aliases = IMB_ALIASES
+        sbt = Sabbat("IMB", aliases, 2026)
+        return sbt
+      case "OST26":
+        aliases = OST_ALIASES
+        sbt = Sabbat("OST", aliases, 2026)
+        return sbt
+      case "BEL26":
+        aliases = BEL_ALIASES
+        sbt = Sabbat("BEL", aliases, 2026)
+        return sbt
+      case "LTH26":
+        aliases = LTH_ALIASES
+        sbt = Sabbat("LTH", aliases, 2026)
+        return sbt
+      case "LMS26":
+        aliases = LMS_ALIASES
+        sbt = Sabbat("LMS", aliases, 2026)
+        return sbt
+      case "MBN26":
+        aliases = MBN_ALIASES
+        sbt = Sabbat("MBN", aliases, 2026)
+        return sbt
+      case "SMH26":
+        aliases = SMH_ALIASES
+        sbt = Sabbat("SMH", aliases, 2026)
+        return sbt
+      case "YUL26":
+        aliases = YUL_ALIASES
+        sbt = Sabbat("YUL", aliases, 2026)
+        return sbt
+      
---
+++ constants_sabbats.py
@@ -0,0 +1,31 @@
+BEL_ALIASES = [
+  "Beltane"
+]
+
+LTH_ALIASES = [
+  "Litha",
+]
+
+LMS_ALIASES = [
+  "Lammas",
+]
+
+MBN_ALIASES = [
+  "Mabon",
+]
+
+SMH_ALIASES = [
+  "Samhain",
+]
+
+YUL_ALIASES = [
+  "Yule",
+]
+
+IMB_ALIASES = [
+  "Imbolc",
+]
+
+OST_ALIASES = [
+  "Ostara",
+]
