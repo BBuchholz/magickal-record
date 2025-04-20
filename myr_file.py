c414929a5e04a36fdb566a1ce4f417fc2c22e044
Brent Buchholz <brentbuchholz@gmail.com>
2025-04-20 09:51:04 -0500

    Add equality comparison operator override to MyrFile class

--- myr_file.py
+++ myr_file.py
@@ -1,6 +1,19 @@
+# Myriad Files are more than "mere files" :)
+# the class name is a play on words
+# much like MyrKis are more than "mere keys"
 class MyrFile:
   def __init__(self):
     self.lines = []
+
+  def __eq__(self, value):
+    my_lines = self.get_lines()
+    those_lines = value.get_lines()
+    if len(my_lines) != len(those_lines):
+      return False
+    for i in range(len(my_lines)):
+      if my_lines[i] != those_lines[i]:
+        return False
+    return True
 
   def get_lines(self):
     return self.lines
--- test_myr_file.py
+++ test_myr_file.py
@@ -15,3 +15,32 @@ class TestMyrFile(unittest.TestCase):
     self.assertEqual(len(mf.get_lines()), 0)
     mf.load_from_lines_arr(lines)
     self.assertEqual(len(mf.get_lines()), 5)
+
+  def test_should_be_equal_if_lines_are_equal(self):
+    lines_one = [
+      "line 1",
+      "",
+      "line 3",
+      "",
+      "line 5"
+    ]
+    lines_two = [
+      "line 1",
+      "",
+      "line 3",
+      "",
+      "line 5"
+    ]
+    mf_one = MyrFile()
+    mf_one.load_from_lines_arr(lines_one)
+    mf_two = MyrFile()
+    mf_two.load_from_lines_arr(lines_two)
+    # should be equal
+    self.assertEqual(mf_one, mf_two)
+    # change lines and test again
+    lines_three = [
+      "line 1"
+    ]
+    mf_two.load_from_lines_arr(lines_three)
+    # should not be equal anymore
+    self.assertNotEqual(mf_one, mf_two)
