13838bb4b9b131694948e970914cf7bb82041912
Brent Buchholz <brentbuchholz@gmail.com>
2025-05-15 04:53:30 -0500

    Move AWLWA material to its own handler and move related tests out from CartIO

--- test_cartio.py
+++ test_cartio.py
@@ -7,68 +7,6 @@ class TestCartIO(unittest.TestCase):
   def setUp(self):
     self.tcfg = NwdTestConfig()
 
-  def test_should_enshrine_AWLWA_carteography(self):
-    # TODO: from Grey Iron Prison days, chart by speaker
-    # should implement all those tables and enshrine
-    # their reproduction in various formats
-    # (YES, this is an open ended goal, mod this test 
-    # and create others as we proceed, 
-    # storing a flash of insight for later)
-    is_implemented = False
-    self.assertTrue(is_implemented)
-
-  def test_should_respect_AWLWA_tree(self):
-    # TODO: make these true
-
-    # 3D == [
-    #         [2,1],
-    #       ]
-
-    # 3ID == [ 
-    #         [2,1],
-    #        ]
-    is_implemented = False
-    self.assertTrue(is_implemented)
-
-  def test_should_respect_AWLWA_valknut(self):
-    # TODO: make these true
-
-    # 9D == [
-    #         [8,1],
-    #         [2,7],
-    #         [6,3],
-    #         [4,5],
-    #       ]
-
-    # 9ID == [
-    #         [4,5],
-    #         [6,3],
-    #         [2,7],
-    #         [8,1],
-    #        ]
-    is_implemented = False
-    self.assertTrue(is_implemented)
-
-  def test_should_translate_AWLWA_terminology(self):
-    # TODO: make these true, should translate both ways
-    # 3D == "Tree Delta",
-    # 3ID == "Tree Inverse Delta", 
-    # 9D == "Nigh Delta", 
-    # 9ID == "Nigh Inverse Delta" 
-    # NUMERIC TERMS
-    # 1 == UNE
-    # 2 == DU
-    # 3 == Tree
-    # 4 == FOE
-    # 5 == FIE
-    # 6 == CIT
-    # 7 == SEB
-    # 8 == A
-    # 9 == Nigh
-    # 0 == ZEE
-    is_implemented = False
-    self.assertTrue(is_implemented)
-
   def test_should_check_for_cartographer_folder(self):
     cg = CartIO(NwdTestConfig())
     self.assertTrue(cg.verify_folder())
