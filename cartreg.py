91dac4d2753e9903d63d6cfcde4901a075880d72
Brent Buchholz <brentbuchholz@gmail.com>
2025-05-15 04:35:25 -0500

    Implement load from verified default for Cart Registry (uses CartIO stored values)

--- cartreg.py
+++ cartreg.py
@@ -1,6 +1,9 @@
 from cfg import Config
 from os import path
 from cartio import CartIO
+from files import (
+  get_lines_from,
+)
 
 class CartRegistry:
   def __init__(self, cfg: Config):
@@ -12,9 +15,18 @@ class CartRegistry:
     msg += "stored configuation"
     print(msg)
     # TODO: check if stored configuration exists
-    if path.exists(self.cartio.cfg.verified_cart_file()):
+    verified_file = self.cartio.cfg.verified_cart_file()
+    if path.exists(verified_file):
       print("verified file found")
-      # TODO: load from that file
+      cart_file = self.load_cart_file_from(verified_file)
+      print(f"selecting file {cart_file}")
+      self.cartio.select_file(cart_file)
+      print("loading registry from CartIO")
+      for card in self.cartio.cards:
+        self.carts.append(card)
+        print(f"loaded card {card}")
+      print(f"{len(self.carts)} cards loaded")
+      
     else:
       print("verified file not found")
       msg = "autoloading is disabled until a verified "
@@ -22,3 +34,23 @@ class CartRegistry:
       msg += "store option in the cart ops menu"
       print(msg)
       
+  def load_cart_file_from(self, verified_file):
+    print(f"loading verified file: {verified_file}")
+    # TODO: load from that file
+    # mimic obsidio.load vaults
+    lines = get_lines_from(verified_file)
+    print("reading from verified file")
+    print("if verified file has multiple lines")
+    print("only last line will be used")
+    print(f"found {len(lines)} line(s)")
+    found = ""
+    for line in lines:
+      found = self.process_verified_file_line(line)
+    return found
+  
+  def process_verified_file_line(self, line):
+    found = ""
+    if line.lower().startswith("cart_file: "):
+      found = line[11:]
+      print(f"found cart_file: {found}")
+    return found
